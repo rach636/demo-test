c7a0cda39b9f368f9234cc872cd601eceae38c40
rach636 <59640829+rach636@users.noreply.github.com>
2026-02-20 11:45:55 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,9 +2,7 @@ provider "aws" {
   region = var.aws_region
 }
 
-##########################
-# Data for Default VPC and Subnets
-##########################
+# Use default VPC
 data "aws_vpc" "default" {
   default = true
 }
@@ -16,15 +14,14 @@ data "aws_subnets" "default" {
   }
 }
 
-##########################
-# Security Group
-##########################
+# Security Group for ECS tasks
 resource "aws_security_group" "ecs_sg" {
   name        = "ecs-sg"
   description = "Allow HTTP traffic to ECS"
   vpc_id      = data.aws_vpc.default.id
 
   ingress {
+    description = "Allow HTTP"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
@@ -43,22 +40,42 @@ resource "aws_security_group" "ecs_sg" {
   }
 }
 
-##########################
 # ECS Cluster
-##########################
 resource "aws_ecs_cluster" "main" {
-  name = "ecs-cluster"
+  name   = "ecs-cluster"
+  region = var.aws_region
 }
 
-##########################
+# IAM Role for ECS task execution
+resource "aws_iam_role" "ecs_task_execution" {
+  name = "ecsTaskExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_attachment" {
+  role       = aws_iam_role.ecs_task_execution.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
 # ECS Task Definition
-##########################
 resource "aws_ecs_task_definition" "app" {
   family                   = "my-app-task"
   cpu                      = var.ecs_task_cpu
   memory                   = var.ecs_task_memory
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
+
+  execution_role_arn = aws_iam_role.ecs_task_execution.arn
 
   container_definitions = jsonencode([
     {
@@ -75,9 +92,7 @@ resource "aws_ecs_task_definition" "app" {
   ])
 }
 
-##########################
 # ECS Service
-##########################
 resource "aws_ecs_service" "app_service" {
   name            = "my-app-service"
   cluster         = aws_ecs_cluster.main.id
@@ -86,8 +101,12 @@ resource "aws_ecs_service" "app_service" {
   launch_type     = "FARGATE"
 
   network_configuration {
-    subnets         = data.aws_subnets.default.ids
-    security_groups = [aws_security_group.ecs_sg.id]
+    subnets          = data.aws_subnets.default.ids
+    security_groups  = [aws_security_group.ecs_sg.id]
     assign_public_ip = true
   }
+
+  deployment_minimum_healthy_percent = 100
+  deployment_maximum_percent         = 200
+  scheduling_strategy                = "REPLICA"
 }
